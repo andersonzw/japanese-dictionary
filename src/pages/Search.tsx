d32e802a66a455e45bf539a1be6667c23d4d7708
Anderson <84068667+andersonzw@users.noreply.github.com>
2024-03-27 20:10:17 +1100

    created store

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,15 +1,31 @@
 import { useEffect, useState } from "react";
 import "./Search.css";
+import { useAppDispatch, useAppSelector } from "../util/hooks";
+import { addToHistory, selectSearchHistory } from "../util/historySlice";
 
+type HistoryObjType = {
+  word: string;
+  date: string;
+};
 export default function () {
+  const dispatch = useAppDispatch();
+  const history = useAppSelector(selectSearchHistory) as HistoryObjType[];
   const [searchField, setSearchField] = useState("");
   useEffect(() => {
-    console.log(searchField);
-  }, [searchField]);
+    history.map((ele) => console.log(ele.word));
+  }, [history]);
 
   const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
+    const todayDate = new Date();
+    const year = todayDate.getFullYear();
+    const month = (todayDate.getMonth() + 1).toString().padStart(2, "0");
+    const day = todayDate.getDate().toString().padStart(2, "0");
+
     e.preventDefault();
-    window.open(`https://jisho.org/search/${searchField}`, "_blank");
+    const searchObject:HistoryObjType = { word: searchField, date: `${year}-${month}-${day}` };
+
+    dispatch(addToHistory(searchObject));
+    //  window.open(`https://jisho.org/search/${searchField}`, "_blank");
   };
 
   return (
@@ -33,6 +49,12 @@ export default function () {
           <button>Search</button>
         </div>
       </form>
+
+      <div className="history-container">
+        {history.map((obj) => {
+          return <div>{obj.word}</div>;
+        })}
+      </div>
     </div>
   );
 }
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Search from "./pages/Search";
+import { store } from "./util/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.body).render(
   <React.StrictMode>
-    <Search />
+    <Provider store={store}>
+      <Search />
+    </Provider>
   </React.StrictMode>
 );
